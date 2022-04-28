8bd10ef1b9f54aa462fd363242b980722205bd4c
seedboxtuga <89316920+seedboxtuga@users.noreply.github.com>
2022-04-27 22:41:32 -0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -5,7 +5,8 @@
 sudo apt update
 clear
 sudo apt install screen
-screen -R xmr
+
+
 
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.4/SRBMiner-Multi-0-9-4-Linux.tar.xz
 tar -xf SRBMiner-Multi-0-9-4-Linux.tar.xz
