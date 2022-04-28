6b21ee333601413341e786393901f9ff68a841a7
seedboxtuga <89316920+seedboxtuga@users.noreply.github.com>
2022-04-27 22:38:20 -0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -11,4 +11,4 @@ screen -R xmr
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.4/SRBMiner-Multi-0-9-4-Linux.tar.xz
 tar -xf SRBMiner-Multi-0-9-4-Linux.tar.xz
 cd SRBMiner-Multi-0-9-4
-./SRBMiner-MULTI --multi-algorithm-job-mode 1 --disable-cpu --algorithm progpow_veil --pool eu.epicmine.org:3334 --tls false --wallet ruph06.worker_console --password 12345678m=pool --keepalive true
+./xmrig -o de.monero.herominers.com:1111 -a rx -k -u 438hdgmCogPSF85d7KZXTETtmodfUpQAm842oY9qymi8SfukFsFfuVEMXaGzzQE7SxN5CqKCCT8QabA9PEQeTJqpM11Dz5w.worker -p x
