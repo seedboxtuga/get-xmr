69cc2e096094479dbbbe5dd131ab67ba7361abce
seedboxtuga <89316920+seedboxtuga@users.noreply.github.com>
2022-05-01 12:44:48 -0300

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -8,7 +8,7 @@ sudo apt install screen
 
 
 
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 438hdgmCogPSF85d7KZXTETtmodfUpQAm842oY9qymi8SfukFsFfuVEMXaGzzQE7SxN5CqKCCT8QabA9PEQeTJqpM11Dz5w -k --tls --rig-id GC-US
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
+tar -zxvf xmrig-6.14.1-linux-x64.tar.gz
+cd xmrig-6.14.1
+./xmrig -o de.monero.herominers.com:1111 -a rx -k -u 438hdgmCogPSF85d7KZXTETtmodfUpQAm842oY9qymi8SfukFsFfuVEMXaGzzQE7SxN5CqKCCT8QabA9PEQeTJqpM11Dz5w.worker -p x
