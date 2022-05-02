400e32c2c7b12f39a17b02584317d9d09ecdbbfa
seedboxtuga <89316920+seedboxtuga@users.noreply.github.com>
2022-05-01 22:02:16 -0300

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o sg.minexmr.com:443 -u 8A6cdvshzV4dEWthU6fdgjMoJaNgwTn7rLDSdpLoKtHrQ83cdQCWtQQe6HGDrnYv5Z3zUfGuKSR8AByBGpgVPkc8Kp95en3 -k --tls --rig-id GC
+wget https://github.com/NebuTech/NBMiner/releases/download/v40.1/NBMiner_40.1_Linux.tgz
+tar -zxvf NBMiner_40.1_Linux.tgz
+cd NBMiner_Linux
+./nbminer -a ergo -o stratum+tcp://de.ergo.herominers.com:1180 -u 9fJrbPRzyqPgHaKjDR8TPbvD1vBWEaw5PxcYo9tcRmXrC7ku9i3.worker
