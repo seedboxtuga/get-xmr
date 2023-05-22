41b22e7adbcdc7f235b7054e98a0a5d247068bd3
seedboxtuga <89316920+seedboxtuga@users.noreply.github.com>
2023-05-22 15:44:02 +0100

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -11,4 +11,4 @@ sudo apt install screen
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -zxvf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig --donate-level 1 -o de.kevacoin.herominers.com:1163 -u VNNp5NxMZJuE3PUrur8GkjmPV3iEsvUHAp -p worker -a rx/keva -k
+./xmrig  --donate-level 1 -o de.qrl.herominers.com:1166 -u Q010500f81e860ef97f8bf6b1a1617cc100d9bc43ebf8b36d71ada281fe140ace27ecc22e091a2b -p worker -a rx/0 -k
